8c2f2c99569bf3c46658d447e1bb3ac7c0592b67
dora <dodoralin03@gmail.com>
2025-11-25 13:44:00 +0800

    p1

--- src/p1.cpp
+++ src/p1.cpp
@@ -2,9 +2,24 @@
 
 #include <iostream>
 using namespace std;
+int factorial(int n){
+    if(n==0 || n==1){
+        return 1;
+    }else return n*factorial(n-1);
+}
+int sum(int n){
+    if(n==0){
+        return 0;
+    }
+    if(n==1){
+        return 1;
+    }else return n+sum(n-1);
+}
 
 int main() {
-    
-    
+    int n;
+    while(cin>>n){
+        cout << factorial(n) << endl << sum(n) << endl;
+    }
     return 0;
 }
