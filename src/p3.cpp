96fe6d8e9d15ccb3509e9e8c3a092ac1de61b18a
dora <dodoralin03@gmail.com>
2025-11-25 14:45:45 +0800

    p3

--- src/p3.cpp
+++ src/p3.cpp
@@ -3,8 +3,25 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    
-    
+bool isPrime(int x){
+    if(x <= 1) return false;
+    for(int i = 2; i * i <= x; i++){
+        if(x % i == 0) return false;
+    }
+    return true;
+}
+
+int main(){
+    int n;
+    cin >> n;
+
+    for(int a = 2; a <= n/2; a++){
+        int b = n - a;
+        if(isPrime(a) && isPrime(b)){
+            cout << a << " " << b;
+            break;
+        }
+    }
+
     return 0;
 }
